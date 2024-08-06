26465f5a471d7dba818369eb5486065422139580
Швецов Сергей Владимирович <shvetsov@elvees.com>
2024-08-06 10:26:38 +0300

    customize python formatter

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -10,11 +10,25 @@ local options = {
 		yaml = { { "prettierd", "prettier" } },
 	},
 
-	-- format_on_save = {
-	--   -- These options will be passed to conform.format()
-	--   timeout_ms = 500,
-	--   lsp_fallback = true,
-	-- },
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_fallback = true,
+		async = false,
+	},
+
+	formatters = {
+		autopep8 = {
+			command = "autopep8",
+			range_args = function(self, ctx)
+				return { "-", "--line-range", tostring(ctx.range.start[1]), tostring(ctx.range["end"][1]) }
+			end,
+			args = {
+				"-",
+				"--ignore=E402",
+			},
+		},
+	},
 }
 
 require("conform").setup(options)
