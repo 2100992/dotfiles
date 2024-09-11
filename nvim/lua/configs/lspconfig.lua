731dc690f0989de90311d304cd865ec23ccc84a3
2100992 <2100992@gmail.com>
2024-09-11 10:31:38 +0300

    rename tsserver to ts_ls

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -8,18 +8,18 @@ local servers = { "html", "cssls" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup({
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  })
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		on_init = on_init,
+		capabilities = capabilities,
+	})
 end
 
 -- typescript
-lspconfig.tsserver.setup({
-  on_attach = on_attach,
-  on_init = on_init,
-  capabilities = capabilities,
+lspconfig["ts_ls"].setup({
+	on_attach = on_attach,
+	on_init = on_init,
+	capabilities = capabilities,
 })
 
 -- vue
@@ -29,42 +29,28 @@ lspconfig.tsserver.setup({
 
 local util = require("lspconfig.util")
 local function get_typescript_server_path(root_dir)
-  local global_ts = "~/.npm/lib/node_modules/typescript/lib"
-  -- Alternative location if installed as root:
-  -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
-  local found_ts = ""
-  local function check_dir(path)
-    found_ts = util.path.join(path, "node_modules", "typescript", "lib")
-    if util.path.exists(found_ts) then
-      return path
-    end
-  end
-  if util.search_ancestors(root_dir, check_dir) then
-    return found_ts
-  else
-    return global_ts
-  end
+	local global_ts = "~/.npm/lib/node_modules/typescript/lib"
+	-- Alternative location if installed as root:
+	-- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
+	local found_ts = ""
+	local function check_dir(path)
+		found_ts = util.path.join(path, "node_modules", "typescript", "lib")
+		if util.path.exists(found_ts) then
+			return path
+		end
+	end
+	if util.search_ancestors(root_dir, check_dir) then
+		return found_ts
+	else
+		return global_ts
+	end
 end
 
 lspconfig.volar.setup({
-  on_new_config = function(new_config, new_root_dir)
-    new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
-  end,
+	on_new_config = function(new_config, new_root_dir)
+		new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+	end,
 })
 
 -- python
--- require("lspconfig").jedi_language_server.setup({})
 lspconfig.pyright.setup({})
-
--- require'lspconfig'.pylsp.setup{
---   settings = {
---     pylsp = {
---       plugins = {
---         pycodestyle = {
---           ignore = {'W391'},
---           maxLineLength = 100
---         }
---       }
---     }
---   }
--- }
