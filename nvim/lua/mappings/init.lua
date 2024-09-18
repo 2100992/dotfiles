1baefcfa5a29ae21f0cb38f267116bad6ebaf757
2100992 <2100992@gmail.com>
2024-09-18 10:40:36 +0300

    add keymapping for DiffView

--- nvim/lua/mappings/init.lua
+++ nvim/lua/mappings/init.lua
@@ -1,10 +1,11 @@
 require("nvchad.mappings")
-
 -- add yours here
 
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
+map("n", "<leader>do", "<cmd>DiffviewOpen<CR>", { desc = "Diffview open" })
+map("n", "<leader>dc", "<cmd>DiffviewClose<CR>", { desc = "Diffview close" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
