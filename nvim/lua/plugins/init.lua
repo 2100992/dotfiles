e23a86939a3d8320659bd72749c06ab1ef0d9c79
2100992 <2100992@gmail.com>
2024-12-13 17:29:01 +0300

    experiment

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -107,8 +107,19 @@ return {
 			require("configs.dap")
 		end,
 	},
+	-- {
+	-- 	"Exafunction/codeium.vim",
+	-- 	event = "BufEnter",
+	-- },
 	{
-		"Exafunction/codeium.vim",
+		"Exafunction/codeium.nvim",
+		requires = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+		},
+		config = function()
+			require("codeium").setup({})
+		end,
 		event = "BufEnter",
 	},
 }
