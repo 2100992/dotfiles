c92f306b863567fa9fe6b9add4bb7ed58173f5b0
2100992 <2100992@gmail.com>
2025-05-06 12:35:26 +0300

    small fix in python debugger

--- nvim/lua/configs/dap.lua
+++ nvim/lua/configs/dap.lua
@@ -33,8 +33,8 @@ table.insert(dap.configurations.python, {
 	-- env = {},
 	justMyCode = true,
 	-- console = "internalConsole",
-	-- console = "integratedTerminal",
-	console = "externalTerminal",
+	console = "integratedTerminal",
+	-- console = "externalTerminal",
 })
 
 table.insert(dap.configurations.python, {
