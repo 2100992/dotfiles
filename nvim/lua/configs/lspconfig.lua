262c70e4ba2b855fc2916fe54a1f13bddc4de154
2100992 <2100992@gmail.com>
2025-07-11 17:52:00 +0300

    move to vue ls 3version

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -24,16 +24,7 @@ lspconfig["ts_ls"].setup({
 	capabilities = capabilities,
 })
 
-lspconfig.volar.setup({
-	-- add filetypes for typescript, javascript and vue
-	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-	init_options = {
-		vue = {
-			-- disable hybrid mode
-			hybridMode = false,
-		},
-	},
-})
+require("configs.lsp.vue")
 
 lspconfig.pyright.setup({
 	on_attach = on_attach,
